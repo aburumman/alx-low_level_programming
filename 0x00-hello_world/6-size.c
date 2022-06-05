e27bc8e63932321c45cba548325ee9cd8b66c712
Alayande Mustapha <alayandemustapha@gmail.com>
2022-06-05 15:05:02 -0700

    completed size of: 2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main: The main function
- * description: Prints the size of various data types
- * Return: (0)
+ * main - Prints the size of thee various data types
+ * Description: Prints the size of various data types
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
