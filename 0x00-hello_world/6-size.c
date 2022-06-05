d20801b76eaacb09e3e901091b90a8b21560edcb
Alayande Mustapha <alayandemustapha@gmail.com>
2022-06-05 14:41:09 -0700

    completed size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,17 @@
  */
 int main(void)
 {
-	char c[];
-	int a;
-	long int b;
-	float c;
-	printf("Size of a char: %i byte(s)", sizeof());
+	int my_int;
+	char mychar;
+	float myfloat;
+	long int long_int;
+	long long int longer_int;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(mychar));
+	printf("Size of an int: %li byte(s)\n", sizeof(my_int));
+	printf("Size of a long int: %li byte(s)\n", sizeof(long_int));
+	printf("Size of a long long int: %li byte(s)\n", sizeof(longer_int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(myfloat));
+
 	return (0);
 }
