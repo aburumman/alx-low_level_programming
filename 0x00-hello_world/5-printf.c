5196ea0d778650ddebba86e5b80160fe5755dfc0
Alayande Mustapha <alayandemustapha@gmail.com>
2022-06-04 17:01:19 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program by me yeah
+ * Description: Completing the task
+ * Return: Return nothing, just zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
