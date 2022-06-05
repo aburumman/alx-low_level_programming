9e35c869ea61d40ccaa9ac296507ae1f7737f0c7
Alayande Mustapha <alayandemustapha@gmail.com>
2022-06-05 01:39:52 -0700

    size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - 
+ * Description:
+ * Return: 
+ */
+int main(void)
+{
+	char c[];
+	int a;
+	long int b;
+	float c;
+	printf("Size of a char: %i byte(s)", sizeof());
+	return (0);
+}
