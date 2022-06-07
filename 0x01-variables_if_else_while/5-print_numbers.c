7e7381a9893eb1822c1fb2bccd8c955e8edab619
Alayande Mustapha <alayandemustapha@gmail.com>
2022-06-06 20:53:19 -0700

    print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 /**
- * Main - Print numbers
- * Description: Prints 0 - 9
- * Return : 0
+ * main - Print numbers 0 - 9
+ * description : Prints integers 0 - 9 in the main function.
+ * Return: Always (0) on success          .
  */
 
 int main(void)
